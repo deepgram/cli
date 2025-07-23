81d7c55f3cd62791b37415f1a933bbcf577b34ac
Luke Oliff <luke@lukeoliff.com>
2025-07-23 13:27:39 +0100

    chore: bump version to v0.1.4

--- src/deepctl/__init__.py
+++ src/deepctl/__init__.py
@@ -1,7 +1,7 @@
 """deepctl - Official command-line interface for Deepgram's speech
 recognition API."""
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Deepgram"
 __email__ = "devrel@deepgram.com"
 __license__ = "MIT"
